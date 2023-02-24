d8b40c666c99b405126ab318afcd2ad5ce28e86f
Erhan <erhantezer68@gmail.com>
2023-02-24 03:39:07 +0300

    updated

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import { data } from "../helpers/data";
 
 const CardContainer = () => {
-    return <div>CardContainer</div>;
+    return (
+            <Container>
+                {data.map((item,index) => {
+                    const { name, img, statistics } = item
+                    return (
+                            
+                        )
+                })}
+            </Container>
+        )
 };
 
 export default CardContainer;
