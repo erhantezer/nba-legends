306f06d5a147a33be6e2c150545b6b5e939c6ad4
Erhan <erhantezer68@gmail.com>
2023-02-25 15:00:08 +0300

    updated

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,18 +1,29 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import { data } from "../helpers/data";
 
 const CardContainer = () => {
+    const [search, setSearch] = useState("");
     return (
-            <Container>
-                {data.map((item,index) => {
-                    const { name, img, statistics } = item
-                    return (
+        <>
+            <Form.Control
+                type="search"
+                placeholder="Search Player..."
+                className="w-50 m-auto"
+                onChange={() => setSearch(e.target.value)}
+            />
+            <Container className="card-container rounded-4 my-4 p-3">
+                <Row className="g-3 justify-content-center">
+                    {data.map((item, index) => {
+                        const { name, img, statistics } = item
+                        return (
                             
                         )
-                })}
+                    })}
+                </Row>
             </Container>
-        )
+        </>
+    )
 };
 
 export default CardContainer;
