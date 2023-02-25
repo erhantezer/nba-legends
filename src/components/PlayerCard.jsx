a0b41b16386ad6d20f7441592f0fa0f5deb08699
Erhan <erhantezer68@gmail.com>
2023-02-25 15:30:27 +0300

    updated

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useState } from "react";
+import { Card } from "react-bootstrap";
 
-const PlayerCard = () => {
-    return <div>PlayerCard</div>;
+const PlayerCard = ({ name, img, statistics }) => {
+    const [showImage, setshowImage] = useState(true);
+    return (
+        <Card
+            onClick={() => setshowImage(!showImage)}
+            className="rounded-2 m-auto player-card"
+            role="button"
+        >
+            {showImage ? (
+                <Card.Img variant="top" src={img} className="player-logo" />
+            ) :  (
+                <ul className="m-auto">
+                    {statistics.map((item, i) => {
+                        return (
+                            <li key={i} className="h5 text-start list-unstyled">
+                                🏀 {item}
+                            </li>
+                            )
+                    })}
+                </ul>
+            )}
+            <Card.Footer>
+                <Card.Title>{name}</Card.Title>
+            </Card.Footer>
+        </Card>
+    )
 };
 
 export default PlayerCard;
