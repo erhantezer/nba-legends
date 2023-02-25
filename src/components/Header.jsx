c48bbba81ed75fbaf6b05d695f04e09ceae64e89
Erhan <erhantezer68@gmail.com>
2023-02-25 15:16:14 +0300

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
+import nbaLogo from "../assets/nba-logo.png";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <Container>
+            <Image src={nbaLogo} width="200px" ></Image>
+            <h1 className="my-2 font-monospace display-4 fw-bold">NBA Legends</h1>
+        </Container>
+        )
 };
 
 export default Header;
